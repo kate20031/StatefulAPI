d17652383a94be44f36696bb33795304541682b3
Kateryna PAVLICHENKO <pavlichk@iut-acy.local>
2026-09-11 15:27:38 +0200

    feat: generation du modele de donnees avec Fluent API

---
+++ StatefulAPI/Models/EntityFramework/Serie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StatefulAPI.Models.EntityFramework;
+
+public partial class Serie
+{
+    public int Serieid { get; set; }
+
+    public string Titre { get; set; } = null!;
+
+    public string? Resume { get; set; }
+
+    public int? Nbsaisons { get; set; }
+
+    public int? Nbepisodes { get; set; }
+
+    public int? Anneecreation { get; set; }
+
+    public string? Network { get; set; }
+}
---
+++ StatefulAPI/Models/EntityFramework/SeriesDbContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace StatefulAPI.Models.EntityFramework;
+
+public partial class SeriesDbContext : DbContext
+{
+    public SeriesDbContext()
+    {
+    }
+
+    public SeriesDbContext(DbContextOptions<SeriesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Serie> Series { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=MonNomDeBase;Username=MonUtilisateur;Password=MonMotDePasse");
+        }
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Serie>(entity =>
+        {
+            entity.HasKey(e => e.Serieid).HasName("serie_pkey");
+
+            entity.ToTable("serie");
+
+            entity.Property(e => e.Serieid).HasColumnName("serieid");
+            entity.Property(e => e.Anneecreation).HasColumnName("anneecreation");
+            entity.Property(e => e.Nbepisodes).HasColumnName("nbepisodes");
+            entity.Property(e => e.Nbsaisons).HasColumnName("nbsaisons");
+            entity.Property(e => e.Network)
+                .HasMaxLength(50)
+                .HasColumnName("network");
+            entity.Property(e => e.Resume).HasColumnName("resume");
+            entity.Property(e => e.Titre)
+                .HasMaxLength(100)
+                .HasColumnName("titre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
